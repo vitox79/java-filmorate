3632b2214d0f3a630f0b2d47883530f3fd4be088
vitox79 <viktor.monarev@gmail.com>
2023-04-04 23:03:33 +0700

    spring 11_7

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -3,8 +3,6 @@ import lombok.Builder;
 import lombok.Data;
 import javax.validation.constraints.*;
 import java.time.LocalDate;
-
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
--- src/main/java/ru/yandex/practicum/filmorate/strorage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/strorage/FilmStorage.java
@@ -1,5 +1,4 @@
 package ru.yandex.practicum.filmorate.strorage;
-
 import ru.yandex.practicum.filmorate.model.*;
 
 import java.util.List;
