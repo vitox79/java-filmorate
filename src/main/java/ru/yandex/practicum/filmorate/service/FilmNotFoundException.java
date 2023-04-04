cebf414f47dd90032b0d68e9e833a45d22b0ae11
vitox79 <viktor.monarev@gmail.com>
2023-04-04 23:19:14 +0700

    spring 11_11

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmNotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmNotFoundException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.service;
 
-public class FilmNotFoundException extends RuntimeException{
+public class FilmNotFoundException extends RuntimeException {
 
     public FilmNotFoundException(String message) {
         super(message);
