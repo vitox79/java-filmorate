72890073363a79601816b5cf1c596ec1068f8e2f
vitox79 <viktor.monarev@gmail.com>
2023-04-04 23:10:42 +0700

    spring 11_9

--- src/main/java/ru/yandex/practicum/filmorate/service/DataNotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/DataNotFoundException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.service;
 
-public class DataNotFoundException extends RuntimeException{
+public class DataNotFoundException extends RuntimeException {
 
     public DataNotFoundException(String message) {
         super(message);
