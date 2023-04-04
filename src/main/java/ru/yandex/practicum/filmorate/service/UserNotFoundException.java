abcc39137b3e4c85ea02bada46409cb8e70c79ab
vitox79 <viktor.monarev@gmail.com>
2023-04-04 23:07:21 +0700

    spring 11_8

--- src/main/java/ru/yandex/practicum/filmorate/service/UserNotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserNotFoundException.java
@@ -1,8 +1,8 @@
 package ru.yandex.practicum.filmorate.service;
 
 public class UserNotFoundException extends RuntimeException {
-        public UserNotFoundException(String message) {
 
+        public UserNotFoundException(String message) {
             super(message);
         }
     }
