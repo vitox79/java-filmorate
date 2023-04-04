dd28b6a868b0e9c839f362dde397dd92c0e35b91
vitox79 <viktor.monarev@gmail.com>
2023-04-05 01:01:17 +0700

    spring 11_17

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,15 +1,16 @@
+INSERT INTO Genres (name)
+SELECT 'Комедия' UNION
+SELECT 'Драма' UNION
+SELECT 'Мультфильм' UNION
+SELECT 'Триллер' UNION
+SELECT 'Документальный' UNION
+SELECT 'Боевик'
+WHERE NOT EXISTS (SELECT 1 FROM Genres WHERE name IN ('Комедия', 'Драма', 'Мультфильм', 'Триллер', 'Документальный', 'Боевик'));
 
-INSERT  INTO Genres (name)
-VALUES ('Комедия'),
-       ('Драма'),
-       ('Мультфильм'),
-       ('Триллер'),
-       ('Документальный'),
-       ('Боевик');
-
-INSERT  INTO MPA (name)
-VALUES ('G'),
-       ('PG'),
-       ('PG-13'),
-       ('R'),
-       ('NC-17');
+INSERT INTO MPA (name)
+SELECT 'G' UNION
+SELECT 'PG' UNION
+SELECT 'PG-13' UNION
+SELECT 'R' UNION
+SELECT 'NC-17'
+WHERE NOT EXISTS (SELECT 1 FROM MPA WHERE name IN ('G', 'PG', 'PG-13', 'R', 'NC-17'));
